c2d23b3c6aed9a9bb95985c5bec2a8a0727ef2f7
Roweens <enderman2011@gmail.com>
2023-02-01 09:50:50 +0300

    first commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,9 @@
+import classes from './Counter.module.scss';
+
+export const Counter = () => {
+  return (
+    <div>
+      <button className={classes.abrakadabra}>Get</button>
+    </div>
+  );
+};
