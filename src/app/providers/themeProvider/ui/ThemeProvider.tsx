ee483ccad00e17eefeef36e0a2fe562df4ac4600
Roweens <enderman2011@gmail.com>
2023-02-15 20:39:27 +0300

    fix

--- src/app/providers/themeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/themeProvider/ui/ThemeProvider.tsx
@@ -7,7 +7,7 @@ import {
 
 const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
 
-const ThemeProvider: FC = ({ children }) => {
+export const ThemeProvider: FC = ({ children }) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme);
 
     const defaultProps = useMemo(
@@ -20,5 +20,3 @@ const ThemeProvider: FC = ({ children }) => {
 
     return <ThemeContext.Provider value={defaultProps}>{children}</ThemeContext.Provider>;
 };
-
-export default ThemeProvider;
