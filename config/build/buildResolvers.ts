56e81dbe40d39f5a6cc70cc9f9e9b0b082ed6bf6
Roweens <enderman2011@gmail.com>
2023-05-15 21:31:38 +0300

    fix deploy

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,12 +3,13 @@ import { BuildOptions } from './types/config';
 
 export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: [ '.js', '.jsx', '.tsx', '.ts'],
         preferAbsolute: true,
         modules: [options.paths.src, 'node_modules'],
         mainFiles: ['index'],
         alias: {
             '@': options.paths.src,
         },
+        symlinks: false
     };
 }
